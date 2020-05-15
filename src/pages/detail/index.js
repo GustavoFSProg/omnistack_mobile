059c74c45d1cb874a4fc47f57df04f8d39a1dd7a
Gustavo <gustavosohne38@gmail.com>
2020-05-15 16:29:27 -0300

    done

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,6 +1,22 @@
 import React from 'react'
-import { View } from 'react-native'
+import styles from '../detail/styles'
+import { View, Image, Text, FlatList, TouchableOpacity } from 'react-native'
+import logoImg from '../../assets/logo.png'
+import { useNavigation } from '@react-navigation/native'
+import { Feather } from '@expo/vector-icons'
 
 export default function Detail() {
-  return <View />
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <Text style={styles.headerText}>
+          Total de Casos<Text style={styles.headerTextBold}>0 casos</Text>
+        </Text>
+      </View>
+      <TouchableOpacity onPress={() => {}}>
+        <Feather name="arrow-left" size={28} color="#E02041"></Feather>
+      </TouchableOpacity>
+    </View>
+  )
 }
