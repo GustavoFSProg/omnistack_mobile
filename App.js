16dbc6869aacea06802510627dea9bc55c014c8f
Gustavo <gustavosohne38@gmail.com>
2020-05-15 15:01:57 -0300

    working

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Routes from './src/routes'
+import { View, Image, Text } from 'react-native'
+import logoImg from '../mobile/src/assets/logo.png'
+import styles from './src/pages/incidents'
+
+export default function App() {
+  return <Routes></Routes>
+}
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+import Incidents from './pages/incidents'
+import Detail from './pages/detail'
+
+const AppStack = createStackNavigator()
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator screenOptions={{ headerShown: false}} initialRouteName="Incidents">
+        <AppStack.Screen name="Incidents" component={Incidents} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  )
+}
