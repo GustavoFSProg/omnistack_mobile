eba5061e92a241a11427513b9dc440956b91f7c7
Gustavo <gustavosohne38@gmail.com>
2020-05-21 19:34:51 -0300

    Done21

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -9,31 +9,37 @@ import {
   Linking,
 } from 'react-native'
 import logoImg from '../../assets/logo.png'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import { Feather } from '@expo/vector-icons'
 import * as MailComposer from 'expo-mail-composer'
+import app from '../../../App'
 
 export default function Detail() {
   const navigation = useNavigation()
+  const route = useRoute()
+
+  const incident = route.params.incident
 
   function navigateToIncidents() {
     // navigation.navigate('Incidents')
     navigation.goBack()
   }
 
-  const mensagem =
-    'Ola Apad, estou entrando em contato pois gostaria de ajudar no caso cadela com o valor de  R$ 120'
+  const mensagem = `Ola  ${incident.ong_name}, estou entrando em contato
+   pois gostaria de ajudar no caso ${incident.title} com o valor de  R$ ${incident.value} `
 
   function sendMail() {
     MailComposer.composeAsync({
-      subject: 'Heroi de uma cadela atropelada.',
-      recipients: ['gustavosohne38@gmail.com'],
+      subject: `Heroi do caso ${incident.title}`,
+      recipients: [incident.email],
       body: mensagem,
     })
   }
 
   function sendWhatsapp() {
-    Linking.openURL(`whatsapp://send?phone=5551984576589?text=${mensagem}`)
+    Linking.openURL(
+      `whatsapp://send?phone=${incident.wathsapp}?text=${mensagem}`
+    )
   }
 
   return (
@@ -42,7 +48,7 @@ export default function Detail() {
         <Image source={logoImg} />
       </View>
 
-      <TouchableOpacity onPress={navigateToIncidents}>
+      <TouchableOpacity style={styles.Button} onPress={navigateToIncidents}>
         <Feather name="arrow-left" size={28} color="#E02041"></Feather>
       </TouchableOpacity>
 
@@ -62,17 +68,20 @@ export default function Detail() {
               <Text style={(styles.incidentProperty, { marginTop: 0 })}>
                 ONG
               </Text>
-              <Text style={styles.incidentValue}> 100 - Viralatas</Text>
+              <Text style={styles.incidentValue}>
+                {' '}
+                {incident.ong_name} de {incident.ong_city}/{incident.ong_uf}
+              </Text>
 
               <Text style={styles.incidentProperty}>Caso:</Text>
-              <Text style={styles.incidentValue}> Cachorro atropelado</Text>
+              <Text style={styles.incidentValue}> {incident.title}</Text>
 
               <Text style={styles.incidentProperty}>Valor</Text>
-              <Text style={styles.incidentValue}> R$ 325</Text>
+              <Text style={styles.incidentValue}>{'R$ ' + incident.value}</Text>
             </View>
 
             <View style={styles.contactBox}>
-              <Text style={styles.heroTitle}>Seja um Heroi para um Pets!</Text>
+              <Text style={styles.heroTitle}>Seja um Heroi para um Pet!</Text>
               <Text style={styles.heroTitle}>Salve um Pet!</Text>
               <Text style={styles.heroDescription}>Entre em contato!</Text>
 
